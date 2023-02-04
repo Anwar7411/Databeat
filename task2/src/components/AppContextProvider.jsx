ac331e26f1f04c985a88c85ecd97b76faea82a65
Anwar Basha <bashaa7411@gmail.com>
2023-02-05 01:32:32 +0530

    added json-server

--- task2/src/components/AppContextProvider.jsx
+++ task2/src/components/AppContextProvider.jsx
@@ -1,16 +1,24 @@
 import { useState, useEffect, createContext } from 'react'
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
 
   const [loginData, setLoginData] = useState({ email: "", password: "" })
+  const[data,setData]=useState([])
   const [isAuth, setIsAuth] = useState(false);
   const [openerror,setOpenError] =useState(false);
   const [opensuccess,setOpenSuccess] =useState(false);
   const [openlogin,setOpenLogin]=useState(false);
 
+  useEffect(()=>{
+  axios.get("http://localhost:8080/user")
+  .then((res)=>{setData(res.data);console.log(res.data)})
+  .catch((err)=>console.log(err))
+  },[])
+   
   const navigate=useNavigate();
 
   const handleChange = (e) => {
@@ -30,10 +38,21 @@ const AppContextProvider = ({ children }) => {
 
   const handleSubmit = () => {
     if (loginData.email && loginData.password) {
-      console.log(loginData)
-        navigate("/")
-        setOpenSuccess(true)
-        setIsAuth(true);
+     let verify= data && data?.filter((el)=>{
+         if(el.email==loginData.email && el.password==loginData.password){
+          return el
+         }
+      })
+      if(verify.length>0){
+
+        console.log(loginData)
+          navigate("/")
+          setOpenSuccess(true)
+          setIsAuth(true);
+      }else{
+        navigate("/signup");
+        setOpenError(true);
+      }
     } else {
       setOpenError(true);
       setLoginData({ email: "", password: "" })
